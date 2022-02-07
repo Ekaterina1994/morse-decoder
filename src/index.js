ecdaef09627c090d19aa3a866e49a2cf648c6e1c
Ekaterina1994 <katishteno@mail.ru>
2022-02-07 23:10:47 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,67 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+   let res = [];
+   let codeArr = [];
+   let codeArray = Object.entries(MORSE_TABLE);
+   let count = 0;
+
+   for (let i = 0; i < expr.length; i += 10) {
+      codeArr.push(expr.slice(i, i + 10));
+   }
+
+   codeArr = codeArr.map(element => {
+      if (element !== "**********") {
+         let arrayPre = new Array(5);
+         for (let i = 0; i < 10; i++) {
+            arrayPre[count] = element.substr(i, 2);
+            count++;
+            i++;
+         }
+         count = 0;
+         return arrayPre;
+      } else {
+         return element;
+      }
+   })
+
+   codeArr = codeArr.map(element => {
+      if (element !== "**********") {
+         element = element.map(element2 => {
+            if (element2 === "00") {
+               return '';
+            } else if (element2 === "10") {
+               return '.';
+            } else if (element2 === "11") {
+               return '-';
+            }
+         });
+         return element;
+      } else {
+         return " ";
+      }
+   })
+
+   codeArr = codeArr.map(element => {
+      if (element !== ' ') {
+         element = element.join('').replace(/,/g, '');
+      } 
+      return element;
+   })
+
+   res = codeArr.map(element => {
+      if (element !== ' ') {
+         codeArray.forEach(element2 => {
+               if (element2[0] === element) {
+                  element = element2[1];
+               }
+         });
+      } 
+      return element;
+   })
+   return res.join('');
 }
 
 module.exports = {
-    decode
+   decode
 }
